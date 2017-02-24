7f564a491f6808056f124f6ec1b1dd32348c83d6
SamGraber <authorsamgraber@gmail.com>
2017-02-24 15:04:23 -0500

    Drop unused packages
    We only need the core angular packages. Also, we don't need to load any css through webpack.

--- app/vendor.ts
+++ app/vendor.ts
@@ -3,10 +3,6 @@ import 'zone.js/dist/zone';
 
 import '@angular/core';
 import '@angular/common';
-import '@angular/http';
-import '@angular/router';
-import '@angular/forms';
 import '@angular/platform-browser-dynamic';
 
 import 'rxjs';
-import 'rxjs/add/operator/map';
