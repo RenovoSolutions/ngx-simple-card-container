f92f26255cbfd418edb5b1b5e3b422e927840565
SamGraber <authorsamgraber@gmail.com>
2017-03-13 14:22:33 -0400

    Implemented default paging in the simple card container
    To do this I'm implementing the concept of 'data filters' in the simple card container. This is essentially anything that we want to apply onto the data before it gets displayed on the page, and thus could be used for sorting as well (if we wanted to implement that). To enable paging, we can simply push the paging 'filter' into the filters list, and this will get applied before the data is displayed to the view. In the absence of any filters, the whole data set will be shown. (This is the normal behavior of lodash reduce)
    
    Also, an advantage to having a single 'process' method that handles all processing is that we can reuse this anywhere we want to force the filtering to re-apply to update the view. At this point, we'll apply on init, anytime the data changes, and anytime the filter changes. The data will likely change once before init, but at that point there wont be any filters, so the 'process' operation should be cheap.

--- lib/container/simpleCardContainer.component.ts
+++ lib/container/simpleCardContainer.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ContentChild } from '@angular/core';
+import { reduce } from 'lodash';
 
 import { IColumn, IPage } from '../interfaces';
 import { CardContentTemplate, CardFooterTemplate, ContainerHeaderTemplate, ContainerFooterTemplate } from '../templates';
+import { PagingService } from '../services/paging.service';
+
+export const defaultPageSize: number = 10;
 
 @Component({
 	selector: 'scc-simple-card-container',
@@ -10,10 +14,21 @@ import { CardContentTemplate, CardFooterTemplate, ContainerHeaderTemplate, Conta
 })
 export class SimpleCardContainerComponent<T> {
 	@Input() columns: IColumn<T>[];
-	@Input() data: T[];
 	@Input() count: number;
 	@Input() pageNumber: number;
+	@Input() pageSize: number;
 	@Input() message: string;
+	@Input() paging: boolean;
+	
+	private _data: T[];
+	@Input() set data(value: T[]) {
+		this._data = value;
+		this.processedData = this.process(value);
+	}
+
+	get data(): T[] {
+		return this._data;
+	}
 
 	get totalItems(): number {
 		return this.count || this.data.length;
@@ -27,12 +42,36 @@ export class SimpleCardContainerComponent<T> {
 	@ContentChild(ContainerHeaderTemplate) containerHeader: ContainerHeaderTemplate;
 	@ContentChild(ContainerFooterTemplate) containerFooter: ContainerFooterTemplate;
 
+	processedData: T[];
 	openCard: T;
 
+	private dataFilters: { (data: T[]): T[] }[];
+
+	constructor(private pagingService: PagingService) {}
+
+	ngOnInit(): void {
+		this.pageNumber = this.pageNumber || 1;
+		this.pageSize = this.pageSize || defaultPageSize;
+
+		this.dataFilters = [];
+
+		if (this.paging) {
+			this.dataFilters.push(data => this.pagingService.filter(data, this.pageNumber, this.pageSize));
+		}
+
+		this.processedData = this.process(this.data);
+	}
+
 	setPage(pageNumber: number): void {
+		this.pageNumber = pageNumber;
+		this.processedData = this.process(this.data);
 		this.page.emit({
-			pageSize: this.data.length,
+			pageSize: this.pageSize,
 			pageNumber,
 		});
+	}
+
+	process(dataSet: T[]): T[] {
+		return reduce(this.dataFilters, (data, filter) => filter(data), dataSet);
 	}
 }
